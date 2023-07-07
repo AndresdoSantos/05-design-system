b2770b898370dcd34720be7ac5f8375696642dab
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-07-06 22:04:00 -0300

    feat: add stitches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,10 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$code',
+  backgroundColor: '$gray600',
+})
+
+export function App() {
+  return <Button />
+}
