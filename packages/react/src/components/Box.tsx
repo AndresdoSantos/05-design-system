2c14ffab7427c75d7e05161c26adbb45db8da3b4
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-07-10 19:29:18 -0300

    feat(components/Text): create Text component

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -1,12 +1,14 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, ElementType } from 'react'
 
 import { styled } from '../styles'
 
 export const Box = styled('div', {
   padding: '$4',
   borderRadius: '$md',
-  backgroundColor: '$gray800',
-  border: '1px solid $gray600',
+  backgroundColor: '$gray100',
+  border: '1px solid $gray200',
 })
 
-export interface BoxProps extends ComponentProps<typeof Box> {}
+export interface BoxProps extends ComponentProps<typeof Box> {
+  as?: ElementType
+}
